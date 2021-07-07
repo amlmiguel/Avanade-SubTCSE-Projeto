b46d74a8481aa25ed5644429699f915ca13f608e
Miguel Leite <amlmiguel@gmail.com>
2021-07-07 11:24:36 -0300

    BaseRepository

--- src/Avanade.SubTCSE.Projeto.Data/Repositories/Employee/EmployeeRepository.cs
+++ src/Avanade.SubTCSE.Projeto.Data/Repositories/Employee/EmployeeRepository.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Avanade.SubTCSE.Projeto.Domain.Aggregates.Employee.Interface.Repositories;
+using Avanade.SubTCSE.Projeto.Infra.Data.Repositories.Base;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,8 @@ using System.Threading.Tasks;
 
 namespace Avanade.SubTCSE.Projeto.Infra.Data.Repositories.Employee
 {
-    public class EmployeeRepository
+    public class EmployeeRepository : BaseRepository<Domain.Aggregates.Employee.Entities.Employee, string>, IEmployeeRepository
     {
+        
     }
 }
--- src/Avanade.SubTCSE.Projeto.Data/Repositories/EmployeeRole/EmployeeRoleRepository.cs
+++ src/Avanade.SubTCSE.Projeto.Data/Repositories/EmployeeRole/EmployeeRoleRepository.cs
@@ -1,12 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using Avanade.SubTCSE.Projeto.Domain.Aggregates.EmployeeRole.Interfaces.Repository;
+using Avanade.SubTCSE.Projeto.Infra.Data.Repositories.Base;
 using System.Threading.Tasks;
 
 namespace Avanade.SubTCSE.Projeto.Infra.Data.Repositories.EmployeeRole
 {
-    public class EmployeeRoleRepository
+    public class EmployeeRoleRepository : BaseRepository<Domain.Aggregates.EmployeeRole.Entities.EmployeeRole, string>, IEmployeeRoleRepository
     {
+        public override Task<Domain.Aggregates.EmployeeRole.Entities.EmployeeRole> Add(Domain.Aggregates.EmployeeRole.Entities.EmployeeRole entity)
+        {
+            //Fazer qualquer coisa
+            entity.RoleName += "sa";
+
+            return base.Add(entity);
+        }
     }
 }
