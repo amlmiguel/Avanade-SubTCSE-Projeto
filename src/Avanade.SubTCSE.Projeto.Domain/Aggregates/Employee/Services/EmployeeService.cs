c07b5b3a4f6c520a4af598f3a1d86f5205d6b1fb
Miguel Leite <amlmiguel@gmail.com>
2021-07-07 08:05:40 -0300

    novo commit

---
+++ src/Avanade.SubTCSE.Projeto.Domain/Aggregates/Employee/Services/EmployeeService.cs
@@ -0,0 +1,14 @@
+﻿using Avanade.SubTCSE.Projeto.Domain.Aggregates.Employee.Interface.Services;
+using System.Threading.Tasks;
+using System;
+
+namespace Avanade.SubTCSE.Projeto.Domain.Aggregates.Employee.Services
+{
+    public class EmployeeService : IEmployeeService
+    {
+        public async Task<Entities.Employee> AddEmployee(Entities.Employee employee)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- src/Avanade.SubTCSE.Projeto.Domain/Aggregates/EmployeeRole/Interfaces/Services/IEmployeeRoleService.cs
+++ src/Avanade.SubTCSE.Projeto.Domain/Aggregates/EmployeeRole/Interfaces/Services/IEmployeeRoleService.cs
@@ -1,7 +1,13 @@
-﻿namespace Avanade.SubTCSE.Projeto.Domain.Aggregates.EmployeeRole.Interfaces.Services
+﻿using Avanade.SubTCSE.Projeto.Domain.Aggregates.EmployeeRole.Interfaces.Services;
+using System.Threading.Tasks;
+
+namespace Avanade.SubTCSE.Projeto.Domain.Aggregates.EmployeeRole.Interfaces.Services
 {
     public interface IEmployeeRoleService
     {
-        void AddEmployeeRole(Entities.EmployeeRole employeeRole);
+        public async Task<Entities.EmployeeRole> AddEmployeeRole(Entities.EmployeeRole employeeRole)
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
